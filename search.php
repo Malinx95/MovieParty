1ce4bafa92b840844b0ce83972e2749c7627990a
e-mgrodet <grodet95@gmail.com>
2021-11-26 15:30:20 +0100

    del search, ajout css pour la grille, a modifier

--- search.php
+++ search.php
@@ -5,7 +5,7 @@
         <meta http-equiv='X-UA-Compatible' content='IE=edge'>
         <title>horraire</title>
         <meta name='viewport' content='width=device-width, initial-scale=1'>
-		<link rel="stylesheet" href="style.css">
+		<link rel="stylesheet" href="main.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
         <script src="function.js"></script>
 </head>
